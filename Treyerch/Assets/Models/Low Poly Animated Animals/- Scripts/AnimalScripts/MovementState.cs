f5bcbe6a2d62717ccc559e3580c26af81d440d61
Nicholas Zehl <njz2587@g.rit.edu>
2020-08-27 19:41:44 -0400

    Model Packs

---
+++ Treyerch/Assets/Models/Low Poly Animated Animals/- Scripts/AnimalScripts/MovementState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LowPolyAnimalPack
+{
+  [Serializable]
+  public class MovementState : AnimalState
+  {
+    public float maxStateTime = 40f;
+    public float moveSpeed = 3f;
+    public float turnSpeed = 120f;
+    public string moveSpeedAnimationFloat;
+  }
+}
