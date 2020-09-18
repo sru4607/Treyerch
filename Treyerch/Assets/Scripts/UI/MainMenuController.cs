0810ecef5452f8a51f6ae4eabd6988f076ffc9cd
Nicholas Zehl <njz2587@g.rit.edu>
2020-09-18 14:50:17 -0400

    Prototype 1 Part 1

---
+++ Treyerch/Assets/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Treyerch/Assets/Scripts/Utils/SceneSwitch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    public string sceneName;
+    public float switchDelay;
+
+    public void SwitchScene()
+    {
+        Invoke("DoSwitch", switchDelay);
+    }
+
+    private void DoSwitch()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
