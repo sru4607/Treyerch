3f765213e0318fdbc8b0b9f65150364cd4be45ec
sru4607 <sru4607@g.rit.edu>
2020-10-08 11:36:14 -0400

    Worked on 3 new levels
    Added work for three levels including scripts, new materials, and
    prefabs

---
+++ Treyerch/Assets/Scripts/LevelScripts/MovingPlatform.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionStay(Collision collision){
+        if(collision.gameObject.tag == "Player"){
+            Debug.Log("Enter");
+            collision.gameObject.transform.parent = gameObject.transform;
+        }
+    }
+
+    void OnCollisionExit(Collision collision){
+        if(collision.gameObject.tag == "Player"){
+            Debug.Log("Left");
+            collision.gameObject.transform.parent = null;
+        }
+
+    }
+}
