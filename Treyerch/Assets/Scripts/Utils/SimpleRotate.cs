346895926c24baf1d2463c4b3ca6d15a5155838b
Alex Pecore <33386022+altonapecore@users.noreply.github.com>
2020-10-09 18:40:03 -0400

    Almost done

---
+++ Treyerch/Assets/Scripts/Utils/SimpleRotate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotate : MonoBehaviour
+{
+    public float degPerSec;
+    public bool cw;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cw)
+        {
+            gameObject.transform.Rotate(0, degPerSec * Time.deltaTime, 0);
+        }
+        else
+        {
+            gameObject.transform.Rotate(0, -degPerSec * Time.deltaTime, 0);
+        }
+    }
+}
