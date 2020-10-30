7b1bb1e414120a8a277102fb54048d0808903f10
Alex Pecore <33386022+altonapecore@users.noreply.github.com>
2020-10-30 14:10:00 -0400

    Finished sprint 3

---
+++ Treyerch/Assets/Scripts/LevelScripts/PenguinSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenguinSpawner : MonoBehaviour
+{
+    public GameObject penguin;
+    private int countNum = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countNum++;
+        if (countNum == 5)
+        {
+            GameObject newPenguin = Instantiate(penguin, gameObject.transform.position, gameObject.transform.rotation);
+            newPenguin.AddComponent<KillPenguin>();
+            countNum = 0;
+        }
+    }
+}
