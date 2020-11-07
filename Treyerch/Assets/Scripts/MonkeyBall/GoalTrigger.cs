8b70fe3e35b76cf463ce79379f132d7819cc0f20
Nicholas Zehl <njz2587@g.rit.edu>
2020-11-06 21:32:46 -0500

    Goal Post fixed

--- Treyerch/Assets/Scripts/MonkeyBall/GoalTrigger.cs
+++ Treyerch/Assets/Scripts/MonkeyBall/GoalTrigger.cs
@@ -7,9 +7,45 @@ public class GoalTrigger : MonoBehaviour
 	public Animator animator;
 	public Transform particleSpawnPoint;
 	public GameObject particleToSpawn;
-	private PlayerController player;
+	public Transform Ribbon;
+	public Vector3 forwardRotation = new Vector3(0, 0, 0);
+	public Vector3 backwardRotation = new Vector3(0, -180, -120);
 
-	private void OnTriggerEnter(Collider c)
+	private PlayerController player;
+	private bool hasTriggered = false;
+
+	private void Update()
+    {
+		if (player)
+		{
+			if (!hasTriggered)
+			{
+				float angle = Vector3.Angle(transform.up, player.transform.position - transform.position);
+				if (Mathf.Abs(angle) < 90)
+				{
+					Ribbon.transform.localRotation = Quaternion.Euler(forwardRotation);
+				}
+				else
+				{
+					Ribbon.transform.localRotation = Quaternion.Euler(backwardRotation);
+				}
+			}
+		}
+		else
+		{
+			if (PlayerController.instance)
+			{
+				player = PlayerController.instance;
+			}
+		}
+    }
+
+    private void OnDrawGizmos()
+    {
+		Gizmos.DrawLine(transform.position, transform.position + (transform.up * 2));
+    }
+
+    private void OnTriggerEnter(Collider c)
 	{
 		if (c.gameObject.layer == 9) //Player
 		{
@@ -20,6 +56,7 @@ public class GoalTrigger : MonoBehaviour
 
 			if (player.isMovable)
 			{
+				hasTriggered = true;
 				animator.SetFloat("Speed", 1);
 				GameObject newParticles = Instantiate(particleToSpawn, particleSpawnPoint.position, particleSpawnPoint.rotation);
 				newParticles.transform.localScale = particleSpawnPoint.localScale;
